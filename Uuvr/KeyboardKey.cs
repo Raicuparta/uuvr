a91c5610fa05262cc372c044cb797f84e5d7efa5
Raicuparta <Raicuparta@users.noreply.github.com>
2024-01-28 12:38:36 +0100

    create unified uuvr plugin project

---
+++ Uuvr/KeyboardKey.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Uuvr;
+
+// Even though Unity has its own Input stuff, there are multiple input systems that can be used,
+// and different Unity versions have slightly different APIs for the same input system.
+// So instead of relying on those systems, we just make our own using native system calls.
+public class KeyboardKey
+{
+    public enum KeyCode
+    {
+        F3 = 0x72,
+        F4 = 0x73,
+        F5 = 0x74,
+    }
+    
+    private bool _previousIsKeyPressed;
+    private bool _isKeyPressed;
+    private readonly KeyCode _keyCode;
+
+    public KeyboardKey(KeyCode keyCode)
+    {
+        _keyCode = keyCode;
+    }
+    
+    public bool UpdateIsDown()
+    {
+        _previousIsKeyPressed = _isKeyPressed;
+        _isKeyPressed = ((ushort)GetKeyState((int)_keyCode) & 0x8000) != 0;
+
+        return !_previousIsKeyPressed && _isKeyPressed;
+    }
+    
+    [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+    private static extern short GetKeyState(int keyCode);
+}
---
+++ Uuvr/UuvrPlugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using UnhollowerRuntimeLib;
+
+namespace Uuvr;
+
+[BepInPlugin("raicuparta.unityuniversalvr", "UUVR", "0.3.0")]
+public class UuvrPlugin : BasePlugin
+{
+    public override void Load()
+    {
+        ClassInjector.RegisterTypeInIl2Cpp<VrCamera>();
+        AddComponent<UuvrCore>();
+    }
+}
