01b3f89b3645718a1c1fca74b95dc88b4496f1ee
Raicuparta <Raicuparta@users.noreply.github.com>
2024-02-24 13:15:35 +0100

    Explain things

--- Uuvr/VrCamera.cs
+++ Uuvr/VrCamera.cs
@@ -38,22 +38,22 @@ public class VrCamera : MonoBehaviour
     // intended camera position. This is annoying, we want tracking relative to the intended position.
     private void DisableTracking()
     {
-        // Type poseDriverType = Type.GetType("UnityEngine.SpatialTracking.TrackedPoseDriver, UnityEngine.SpatialTracking");
-        // if (poseDriverType == null)
-        // {
+        Type poseDriverType = Type.GetType("UnityEngine.SpatialTracking.TrackedPoseDriver, UnityEngine.SpatialTracking");
+        if (poseDriverType == null)
+        {
             // Calling this method disables tracking for some reason.
             // But when I tested it in Aragami, it also messed up the shadows in the right eye.
             // Still, this method is useful for games where TrackedPoseDriver isn't available.
             _camera.SetStereoViewMatrix(Camera.StereoscopicEye.Left, Matrix4x4.zero);
-        // }
-        // else
-        // {
-        //     // Adding a TrackedPoseDriver component, and then disabling it, also disables auto-tracking.
-        //     // This works in Aragami since the TrackedPoseDriver component exists,
-        //     // and it doesn't have the same shadows bug that the SetStereoViewMatrix method caused.
-        //     Component poseDriver = _camera.gameObject.AddComponent(poseDriverType);
-        //     poseDriverType.GetProperty("enabled").SetValue(poseDriver, false, null);
-        // }
+        }
+        else
+        {
+            // Adding a TrackedPoseDriver component, and then disabling it, also disables auto-tracking.
+            // This works in Aragami since the TrackedPoseDriver component exists,
+            // and it doesn't have the same shadows bug that the SetStereoViewMatrix method caused.
+            Component poseDriver = _camera.gameObject.AddComponent(poseDriverType);
+            poseDriverType.GetProperty("enabled").SetValue(poseDriver, false, null);
+        }
     }
 
     private void OnPreCull()
@@ -75,6 +75,9 @@ public class VrCamera : MonoBehaviour
         // I expected SetStereoViewMatrix by itself to be enough, but it was even more broken (although culling did work).
         // So I'm just doing both I guess.
         _camera.worldToCameraMatrix = _trackingCamera.GetStereoViewMatrix(eye);
-        _camera.SetStereoViewMatrix(eye, _camera.worldToCameraMatrix);
+        
+        // Some times setting worldToCameraMatrix is enough, some times not. I'm not sure why, need to learn more.
+        // Some times it's actually better not to call SetStereoViewMatrix, since it messes up the shadows. Like in Aragami.
+        // _camera.SetStereoViewMatrix(eye, _camera.worldToCameraMatrix);
     }
 }
