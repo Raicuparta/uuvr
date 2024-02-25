9b2aa51095dfe64b9ac15bf6bef93b60617fc1a9
Raicuparta <Raicuparta@users.noreply.github.com>
2024-02-25 22:34:26 +0100

    set up ui only when needed

--- Uuvr/UuvrCore.cs
+++ Uuvr/UuvrCore.cs
@@ -4,8 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.UI;
 using UnityEngine.UI.Collections;
+using UnityEngine.XR;
+using UnityEngine.XR.Management;
 
 namespace Uuvr;
 
@@ -49,29 +52,6 @@ public class UuvrCore: MonoBehaviour
         SetXrEnabled(false);
         SetPositionTrackingEnabled(false);
 
-        _uiTexture = new RenderTexture(Screen.width, Screen.height, 0);
-
-        _uiCaptureCamera = new GameObject("VrUiCaptureCamera").AddComponent<Camera>();
-        _uiCaptureCamera.transform.parent = transform;
-        _uiCaptureCamera.clearFlags = CameraClearFlags.Depth;
-        _uiCaptureCamera.targetTexture = _uiTexture;
-        
-        // Dumb solution to avoid the scene camera from seeing the capture camera.
-        // Should use layers instead but dunno maybe not.
-        _uiCaptureCamera.transform.localPosition = Vector3.right * 1000;
-
-        _uiSceneCamera = new GameObject("VrUiSceneCamera").AddComponent<Camera>();
-        _uiSceneCamera.transform.parent = transform;
-        _uiSceneCamera.clearFlags = CameraClearFlags.Depth;
-
-        GameObject vrUiQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        vrUiQuad.name = "VrUiQuad";
-        vrUiQuad.transform.parent = transform;
-        vrUiQuad.transform.localPosition = Vector3.forward * 2f;
-        vrUiQuad.transform.localScale = new Vector3(1.6f, 0.9f, 1f);
-        vrUiQuad.GetComponent<Renderer>().material = Canvas.GetDefaultCanvasMaterial();
-        vrUiQuad.GetComponent<Renderer>().material.mainTexture = _uiTexture;
-
         _graphicRegistryGraphics = GraphicRegistry.instance.GetValue<object>("m_Graphics");
         _graphicRegistryKeysProperty = _graphicRegistryGraphics.GetType().GetProperty("Keys");
         
@@ -88,9 +68,53 @@ public class UuvrCore: MonoBehaviour
 
         UpdateXrUi();
     }
+
+    private void SetUpUi()
+    {
+        // In some cases it might be useful to base the render texture dimensions on HMD,
+        // but I'm guessing that's rare.
+        // _uiTexture = new RenderTexture(XRSettings.eyeTextureWidth, XRSettings.eyeTextureHeight, 16, RenderTextureFormat.ARGB32);
+        _uiTexture = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
+        float uiTextureAspectRatio =  (float) _uiTexture.height / _uiTexture.width;
+
+        _uiCaptureCamera = new GameObject("VrUiCaptureCamera").AddComponent<Camera>();
+        _uiCaptureCamera.transform.parent = transform;
+        _uiCaptureCamera.clearFlags = CameraClearFlags.SolidColor;
+        _uiCaptureCamera.backgroundColor = Color.clear;
+        _uiCaptureCamera.targetTexture = _uiTexture;
+        _uiCaptureCamera.depth = 100;
+        
+        // Dumb solution to avoid the scene camera from seeing the capture camera.
+        // Should use layers instead but dunno maybe not.
+        _uiCaptureCamera.transform.localPosition = Vector3.right * 1000;
+
+        _uiSceneCamera = new GameObject("VrUiSceneCamera").AddComponent<Camera>();
+        _uiSceneCamera.transform.parent = transform;
+        _uiSceneCamera.clearFlags = CameraClearFlags.Depth;
+        _uiSceneCamera.depth = 100;
+
+        GameObject vrUiQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        vrUiQuad.name = "VrUiQuad";
+        vrUiQuad.transform.parent = transform;
+        vrUiQuad.transform.localPosition = Vector3.forward * 2f;
+        float quadWidth = 1.8f;
+        float quadHeight = quadWidth * uiTextureAspectRatio;
+        vrUiQuad.transform.localScale = new Vector3(quadWidth, quadHeight, 1f);
+        vrUiQuad.GetComponent<Renderer>().material = Canvas.GetDefaultCanvasMaterial();
+        vrUiQuad.GetComponent<Renderer>().material.mainTexture = _uiTexture;
+    }
     
     private void ToggleXrUi()
     {
+        bool xrEnabled = (bool) _xrEnabledProperty.GetValue(null, null);
+        if (!xrEnabled)
+        {
+            Debug.LogWarning("Can't toggle VR UI while VR is disabled");
+            return;
+        }
+        
+        if (_uiTexture == null) SetUpUi();
+        
         _shouldPatchUi = !_shouldPatchUi;
     }
 
