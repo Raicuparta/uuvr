6ccba633b4c04b91d5124736d7b805a27c965573
Raicuparta <Raicuparta@users.noreply.github.com>
2024-03-11 18:28:09 +0100

    take canvas plane distance into account

--- Uuvr/VrUiCanvas.cs
+++ Uuvr/VrUiCanvas.cs
@@ -16,6 +16,7 @@ public class VrUiCanvas: UuvrBehaviour
     private bool _isPatched;
     private RenderMode _originalRenderMode;
     private Camera _originalWorldCamera;
+    private float _originalPlaneDistance;
     private int _originalLayer;
 
     public static void Create(Canvas _canvas, Camera uiCaptureCamera)
@@ -63,10 +64,34 @@ public class VrUiCanvas: UuvrBehaviour
         
         _originalRenderMode = _canvas.renderMode;
         _originalWorldCamera = _canvas.worldCamera;
+        _originalPlaneDistance = _canvas.planeDistance;
         _originalLayer = _canvas.gameObject.layer;
         
         _canvas.renderMode = RenderMode.ScreenSpaceCamera;
         _canvas.worldCamera = _uiCaptureCamera;
+
+        if (_originalRenderMode == RenderMode.ScreenSpaceCamera)
+        {
+            // If the canvas was rendering to another camera,
+            // the original plane distance might not fit inside our UI camera frustum.
+            // I'd rather not change the plane distance since it might be important for sorting.
+            // So I'll change our UI camera near/far to make sure it can see all these canvases.
+            if (_originalPlaneDistance < _uiCaptureCamera.nearClipPlane)
+            {
+                _uiCaptureCamera.nearClipPlane = Mathf.Max(0.01f, _originalPlaneDistance - 0.1f);
+            }
+            if (_originalPlaneDistance > _uiCaptureCamera.farClipPlane)
+            {
+                _uiCaptureCamera.farClipPlane = _originalPlaneDistance + 0.1f;
+            }
+        }
+        else
+        {
+            // Doesn't really make sense to enter this,
+            // but if we do, change the distance to something.
+            _canvas.planeDistance = 1f;
+        }
+
         _isPatched = true;
     }
 
@@ -79,6 +104,7 @@ public class VrUiCanvas: UuvrBehaviour
 
         _canvas.renderMode = _originalRenderMode;
         _canvas.worldCamera = _originalWorldCamera;
+        _canvas.planeDistance = _originalPlaneDistance;
         _isPatched = false;
     }
 }
